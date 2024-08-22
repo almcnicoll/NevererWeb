8cb1732c63ef9d4b1575cb327cccf56d8345f3ed
Al McNicoll <github@almcnicoll.co.uk>
2024-08-22 14:40:36 +0100

    Update index.php
    Allowed for domains other than pages/ (must be explicitly allowed in code first)
    Had a use case in mind but decided not to go with it - left functionality in case useful

--- index.php
+++ index.php
@@ -41,8 +41,12 @@ if ($pageinfo->authSetting === PageInfo::AUTH_EARLY) {
     User::loginCheck($pageinfo->redirectOnFail);
 }
 
-
-$page = "pages/{$stub}.php";
+$allowed_domains = ['pages'];
+$domain = 'pages';
+if (isset($_REQUEST['domain'])) {
+    if (in_array($_REQUEST['domain'], $allowed_domains, true)) { $domain = $_REQUEST['domain']; }
+}
+$page = "{$domain}/{$stub}.php";
 
 ob_start(); // Required, as we're including login-check further down
 
@@ -76,6 +80,12 @@ if (!isset($_SESSION['PAGE_LOADCOUNTS'])) {
     <title><?= (substr($config['root_path'],0,strlen('http://localhost'))=='http://localhost' ? 'LOCAL ':'') ?>Neverer</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <link href="<?= $config['root_path'] ?>/css/app.css" rel="stylesheet">
+    <script type='text/javascript'>
+        <!-- Variables from PHP script -->
+        <?php
+        echo "\t\tvar root_path = \"{$config['root_path']}\";\n";
+        ?>
+    </script>
 </head>
 <body>
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
