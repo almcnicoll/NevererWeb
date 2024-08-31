b25a4a742350a3dc17fd73defa19157157298cd8
Al McNicoll <github@almcnicoll.co.uk>
2024-08-31 15:21:26 +0100

    Noted error as TODO

--- class/PlacedClue_List.php
+++ class/PlacedClue_List.php
@@ -8,6 +8,7 @@ class PlacedClue_List extends Typed_List {
 
   // called when accessed like foreach($list as $item) { // $item is type PlacedClue }
   public function current() : PlacedClue {
+    // TODO [31-Aug-2024 14:17:35 UTC] PHP Fatal error:  Uncaught Error: Cannot call abstract method Typed_List::current() in C:\Bitnami\wampstack-8.0.9-0\apache2\htdocs\neverer-web\class\PlacedClue_List.php:11
     return parent::current($this->_position);
   }
 
