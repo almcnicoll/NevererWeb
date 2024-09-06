d66af8cea8659ac11ec5abb50f416ba826952c95
Al McNicoll <almcnicoll@gmail.com>
2024-09-06 09:58:03 +0100

    Added TODO
    TODO - tidy up classes into namespaces and folders

--- class/IllegalClueOverlapException.php
+++ class/IllegalClueOverlapException.php
@@ -1,4 +1,5 @@
 <?php
+// TODO - HIGH PRIORITY Class tidying - Tidy classes into a folder structure instead of a single flat folder, using namespaces
 /** Represents an exception thrown because a clue overlaps with another clue in a way that is not permitted */
 class IllegalClueOverlapException extends Exception {
     //
