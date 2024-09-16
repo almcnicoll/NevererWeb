4fc50986d3310cfbbdca6bf473c26224e5df53b4
Al McNicoll <almcnicoll@gmail.com>
2024-09-17 00:06:14 +0100

    Update BootstrapModal.php
    Fixed an incorrect data attribute

--- class/UI/BootstrapModal.php
+++ class/UI/BootstrapModal.php
@@ -91,7 +91,7 @@ namespace UI {
             <div class="modal-content">
                 <div class="modal-header">
                 <h5 class="modal-title" id="{$this->id}Label">{$this->titleHtml}</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
                 </div>
