88d1679ed78565e07ca6c1ebff1b587273bd7c8d
Al McNicoll <almcnicoll@gmail.com>
2025-07-23 11:43:57 +0100

    Removed completed TODO

--- js/crossword_edit.js
+++ js/crossword_edit.js
@@ -219,7 +219,6 @@ function updateGridSquares(json) {
  */
 function updateClueList(json, removeMissing=true) {
     // TODO - HIGH - display something if clue has no question (answer in italics?)
-    // TODO - HIGH - doesn't correctly update clue list after adding a clue
     // TODO - HIGH - doesn't currently remove clues after a clue-delete (code doesn't even seem to call this function)
     
     // Loop through the list of PlacedClues, updating as we go
