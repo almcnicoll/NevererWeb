0f2b9a2bc03349a41ebf3b4f198269c1fb6084f1
Al McNicoll <github@almcnicoll.co.uk>
2025-08-11 23:11:33 +0100

    Got gradual sync partially working
    Noted bug in Tome::readableBy() with TODO

--- ajax/tome_entry.php
+++ ajax/tome_entry.php
@@ -16,6 +16,11 @@ function throw_error($errors) {
     die(json_encode($retval));
 }
 function populate_from_request($varnames) {
+    if (!is_array($varnames)) {
+        LoggedError::log(LoggedError::TYPE_DEBUG, 0, __FILE__, __LINE__, 
+        "populate_from_request normally receives an array of variable names, but a scalar was provided "
+        ."- this may indicate erroneous usage");
+    }
     foreach ($varnames as $varname) {
         if (isset($_REQUEST[$varname])) { 
             $safe_varname = str_replace('-','_',$varname);
@@ -41,7 +46,7 @@ switch ($action) {
         $permissions_checked = false;
         // Called as /ajax/tome/*/list?tome_ids=[m,n]&limit=p&offset=q
         // If tome_id is not supplied, retrieve for all user-accessible dictionaries
-        populate_from_request('tome_ids','since','limit','offset');
+        populate_from_request(['tome_ids','since','limit','offset']);
         if (!isset($since)) { $since = new DateTime('1970-01-01 00:00:00'); }
         if (!isset($tome_ids)) {
             // Not supplied - retrieve all accessible dictionaries
